576856d382e09c2a59341911dcebad3b34a34cbf
Eugene Melamud <eugene@calicolabs.com>
2016-06-02 08:57:59 -0700

    Swich to Qt 5.6

--- bin/macdeploy.sh
+++ bin/macdeploy.sh
@@ -2,7 +2,9 @@
 DATE=`date +%Y%m%d`
 SRC=/Users/eugenemelamud/src/mzroll
 BUILD=/Users/eugenemelamud/src/mzroll
-QTBIN=/Users/eugenemelamud/Qt/5.5/clang_64/bin/
+#QTBIN=/Users/eugenemelamud/Qt5.7.0/5.7/clang_64/bin
+QTBIN=~/Qt/5.6/clang_64/bin/
+#QTBIN=~/Qt/5.6/clang_64/bin/macdeployqt
 
 #get the latest source
 cd $BUILD
@@ -14,7 +16,7 @@ rm -rf $BUILD/bin/peakdetector.app
 #update Makefile
 $QTBIN/qmake -r
 #make
-make clean
+#make clean
 make -j10
 
 #copy support files
@@ -32,4 +34,4 @@ cp $SRC/bin/scripts/* Maven.app/Contents/Resources/scripts
 #fix Qt dynamic library dependancy
 $QTBIN/macdeployqt Maven.app -dmg
 $QTBIN/macdeployqt peakdetector.app -dmg
-mv Maven.dmg ~/Desktop/Maven_$DATE.dmg
+mv Maven.dmg ~/desktop/Maven_$DATE.dmg
