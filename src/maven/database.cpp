7d87e80b601c8e657bb1216e10d5c46b08a8be38
Phillip Seitzer <phillipseitzer@calicolabs.com>
2019-08-01 10:16:49 -0700

    Issue 39: Implement disconnect() method for ligandDB.

--- src/maven/database.cpp
+++ src/maven/database.cpp
@@ -15,6 +15,25 @@ bool Database::connect(QString filename) {
     }
 }
 
+bool Database::disconnect() {
+
+    qDebug() << "Disconnecting ligand.db";
+
+    ligandDB.close();
+
+    for (auto connection : ligandDB.connectionNames()) {
+        qDebug() << "Removing connection " << connection;
+        QSqlDatabase::removeDatabase(connection);
+    }
+
+    if (ligandDB.isOpen()) {
+        qDebug() << "Failed to close ligand database.";
+        return false;
+    } else {
+        return true;
+    }
+}
+
 void Database::reloadAll() {
 
 	//compounds subsets
@@ -69,8 +88,7 @@ void Database::closeAll() {
     mzUtils::delete_all(compoundsDB);   compoundsDB.clear();
     mzUtils::delete_all(fragmentsDB);   fragmentsDB.clear();
 
-    ligandDB.close();
-    ligandDB.removeDatabase(QSqlDatabase::defaultConnection);
+    disconnect();
 }
 
 multimap<string,Compound*> Database::keywordSearch(string needle) { 
