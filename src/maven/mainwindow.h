d1940994417d1533e313bbef52849840b464ec57
Phillip Seitzer <phillipseitzer@calicolabs.com>
2019-09-23 09:49:24 -0700

    Issue 48: Fix typo in comment.

--- src/maven/mainwindow.h
+++ src/maven/mainwindow.h
@@ -74,7 +74,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     QSettings* getSettings() { return settings; }
-    vector <mzSample*> samples;		//list of loadded samples
+    vector <mzSample*> samples;		//list of loaded samples
     static mzSample* loadSampleStatic(QString filename);
 
     QDoubleSpinBox 	  *ppmWindowBox;
