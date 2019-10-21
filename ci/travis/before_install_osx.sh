5a168bcd71993cb7ecfaeaaf3c7a65ebbcb8cbe0
Phillip Seitzer <phillipseitzer@calicolabs.com>
2019-10-21 16:07:45 -0700

    Updated shell script, returning to downloading and installing qt in travis os x.

--- ci/travis/before_install_osx.sh
+++ ci/travis/before_install_osx.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Debugging: try doing Nothing for mac os x"
+#echo "Maven debugging: try doing nothing for mac os x"
 
-# brew update
-# brew install qt5
-# brew link qt5 --force
-# 
-# ENVFILE=qt-5.env
-# echo Create $ENVFILE
-# cat << EOF > $ENVFILE
-# export PATH="/usr/local/opt/qt/bin:$PATH"
-# export LDFLAGS="-L/usr/local/opt/qt/lib"
-# export CPPFLAGS="-I/usr/local/opt/qt/include"
-# export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig"
-# EOF
+brew update
+brew install qt5
+brew link qt5 --force
+
+ENVFILE=qt-5.env
+echo Create $ENVFILE
+cat << EOF > $ENVFILE
+export PATH="/usr/local/opt/qt/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/qt/lib"
+export CPPFLAGS="-I/usr/local/opt/qt/include"
+export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig"
+EOF
