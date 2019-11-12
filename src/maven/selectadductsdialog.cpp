1200c748e9b3f1426f961c61d3728d58385be9e3
Phillip Seitzer <phillipseitzer@calicolabs.com>
2019-11-12 11:11:51 -0800

    Issue 76: Fix column widths in adducts table.

--- src/maven/selectadductsdialog.cpp
+++ src/maven/selectadductsdialog.cpp
@@ -4,11 +4,15 @@ SelectAdductsDialog::SelectAdductsDialog(QWidget *parent) : QDialog(parent) {
     setupUi(this);
     setModal(true);
 
+    tblAdducts->setColumnWidth(0, 70);      // enabled
+    tblAdducts->setColumnWidth(1, 300);     // name
+    tblAdducts->setColumnWidth(2, 70);      // nmol
+    tblAdducts->setColumnWidth(3, 190);     // mass (sum = 630 px)
+    tblAdducts->setColumnWidth(4, tblAdducts->width()-630-2);      // z (add 2 px for padding)
+
 }
 
-SelectAdductsDialog::~SelectAdductsDialog(){
-
-}
+SelectAdductsDialog::~SelectAdductsDialog(){}
 
 void SelectAdductsDialog::show() {
     QDialog::show();
