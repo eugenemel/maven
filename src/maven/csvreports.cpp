b40059c8f26be610c7fac6b8dc07eda872133f9b
Phillip Seitzer <phillipseitzer@calicolabs.com>
2020-02-04 10:28:29 -0800

    Issue 147: Add compound molecular formula and SMILES string to peak group export.

--- src/maven/csvreports.cpp
+++ src/maven/csvreports.cpp
@@ -31,6 +31,8 @@ void CSVReports::openGroupReport(string outputfile) {
                         << "displayName"
                         << "compound"
                         << "compoundId"
+                        << "compoundFormula"
+                        << "compoundSMILES"
                         << "adductName"
                         << "category"
                         << "database"
@@ -114,8 +116,7 @@ void CSVReports::writeGroupInfo(PeakGroup* group) {
 
     string idString = TableDockWidget::groupTagString(group).toStdString();
 
-    groupReport << group->getPeakGroupLabel() << SEP
-                << setprecision(7)
+    groupReport << group->getPeakGroupLabel() << SEP << setprecision(7)
                 << group->metaGroupId << SEP
                 << group->groupId << SEP
                 << doubleQuoteString(idString) << SEP //header is titled 'ID'
@@ -130,6 +131,8 @@ void CSVReports::writeGroupInfo(PeakGroup* group) {
     string compoundCategory;
     string compoundID;
     string database;
+    string compoundMolecularFormula;
+    string compoundSMILES;
     float  expectedRtDiff=1000;
     float  ppmDist=1000;
 
@@ -140,7 +143,14 @@ void CSVReports::writeGroupInfo(PeakGroup* group) {
         compoundName = c->name;
         compoundID =  c->id;
         database = c->db;
+        compoundMolecularFormula = c->formula;
+        compoundSMILES = c->smileString;
         if (c->category.size()) compoundCategory = c->category[0];
+    }
+
+    //Issue 142: imported compound name always takes precedence
+    if (!group->importedCompoundName.empty()){
+        compoundName = group->importedCompoundName;
     }
 
     string adductName;
@@ -150,6 +160,8 @@ void CSVReports::writeGroupInfo(PeakGroup* group) {
 
     groupReport << SEP << doubleQuoteString(compoundName);
     groupReport << SEP << doubleQuoteString(compoundID);
+    groupReport << SEP << doubleQuoteString(compoundMolecularFormula);
+    groupReport << SEP << doubleQuoteString(compoundSMILES);
     groupReport << SEP << doubleQuoteString(adductName);
     groupReport << SEP << compoundCategory;
     groupReport << SEP << database;
