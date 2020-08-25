fd01043ec2ccc55706c3d0551a2280da911908dc
Phillip Seitzer <phillipseitzer@calicolabs.com>
2020-08-25 11:54:08 -0700

    Issue 278: Added window flags hint, force dialog to top of stack.

--- src/maven/editpeakgroupingdialog.cpp
+++ src/maven/editpeakgroupingdialog.cpp
@@ -4,6 +4,9 @@ EditPeakGroupDialog::EditPeakGroupDialog(QWidget *parent) : QDialog(parent) {
     setupUi(this);
     setModal(true);
 
+    //Issue 278: force window to top
+    setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
+
     brsSuggestions->setOpenLinks(false);
     brsSuggestions->setOpenExternalLinks(false);
     connect(brsSuggestions, SIGNAL(anchorClicked(QUrl)), this, SLOT(onAnchorClicked(QUrl)));
@@ -14,7 +17,13 @@ EditPeakGroupDialog::~EditPeakGroupDialog(){
 }
 
 void EditPeakGroupDialog::show(){
-    QDialog::show();
+    if (QDialog::isVisible()){
+        QDialog::show();
+        QDialog::raise();
+        QDialog::activateWindow();
+    } else {
+        QDialog::show();
+    }
 }
 
 void EditPeakGroupDialog::onAnchorClicked(const QUrl &link){
