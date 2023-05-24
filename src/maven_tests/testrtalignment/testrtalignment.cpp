7aeba55075ca6f531c5ac309ca3f1499535e0a50
Phillip Seitzer <phillipseitzer@calicolabs.com>
2023-05-24 10:47:07 -0700

    Issue 641: Connect loading samples to mzrollDB import.

--- src/maven_tests/testrtalignment/testrtalignment.cpp
+++ src/maven_tests/testrtalignment/testrtalignment.cpp
@@ -26,6 +26,7 @@
 //fields
 Database DB;
 static string mzrollDBFile = "";
+static string sampleDir = "";
 static int groupIdOfInterest = -1;
 
 //functions
@@ -34,7 +35,25 @@ void processCLIArguments(int argc, char* argv[]);
 int main(int argc, char* argv[]) {
     processCLIArguments(argc, argv);
 
-    cout << "Importing mzrolldB file: '" << mzrollDBFile << "'" << endl;
+    cout << "Started importing mzrolldB file: '" << mzrollDBFile << "'..." << endl;
+
+    ProjectDB* selectedProject = new ProjectDB(QString(mzrollDBFile.c_str()));
+
+    vector<mzSample*> samples{};
+
+    QSqlQuery query(selectedProject->sqlDB);
+    query.exec("select * from samples");
+    while (query.next()) {
+        QString sname   = query.value("name").toString();
+        QString fullPath = QString(sampleDir.c_str()).append("/").append(sname);
+
+        mzSample *sample = new mzSample();
+        sample->loadSample(fullPath.toStdString().c_str());
+
+        cout << "Loaded Sample: '" << sname.toStdString() << "'" << endl;
+    }
+
+    cout << "Finished importing mzrolldB file: '" << mzrollDBFile << "'" << endl;
 
     cout << "Test Passed" << endl;
 }
@@ -45,6 +64,8 @@ void processCLIArguments(int argc, char* argv[]){
             mzrollDBFile = argv[i+1];
         } else if (strcmp(argv[i], "--groupId") == 0) {
             groupIdOfInterest = stoi(argv[i+1]);
+        } else if (strcmp(argv[i], "--sampleDir") == 0) {
+            sampleDir = argv[i+1];
         }
     }
 }
