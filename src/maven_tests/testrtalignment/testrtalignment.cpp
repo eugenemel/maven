831c93c325cba3f11e1c372c6bae2e74adeb45b4
Phillip Seitzer <phillipseitzer@calicolabs.com>
2023-05-24 10:56:43 -0700

    Issue 641: Implemented RT alignment in test code.

--- src/maven_tests/testrtalignment/testrtalignment.cpp
+++ src/maven_tests/testrtalignment/testrtalignment.cpp
@@ -37,23 +37,84 @@ int main(int argc, char* argv[]) {
 
     cout << "Started importing mzrolldB file: '" << mzrollDBFile << "'..." << endl;
 
+    /*
+     * Taken from ProjectDockWidget::loadProjectSQLITE()
+     */
     ProjectDB* selectedProject = new ProjectDB(QString(mzrollDBFile.c_str()));
 
     vector<mzSample*> samples{};
+    map<int, mzSample*> sampleById{};
 
     QSqlQuery query(selectedProject->sqlDB);
     query.exec("select * from samples");
     while (query.next()) {
+
         QString sname   = query.value("name").toString();
+        int sampleId  =   query.value("sampleID").toInt();
+        int sampleOrder  = query.value("sampleOrder").toInt();
+
         QString fullPath = QString(sampleDir.c_str()).append("/").append(sname);
 
         mzSample *sample = new mzSample();
         sample->loadSample(fullPath.toStdString().c_str());
+        sample->setSampleId(sampleId);
+        sample->setSampleOrder(sampleOrder);
+
+        sampleById.insert(make_pair(sampleId, sample));
+        samples.push_back(sample);
 
         cout << "Loaded Sample: '" << sname.toStdString() << "'" << endl;
     }
 
+
+    /*
+     * Taken from ProjectDockWidget::doAlignment()
+     */
+    query.exec("select S.name, S.sampleId, R.* from rt_update_key R, samples S where S.sampleId = R.sampleId ORDER BY sampleId, rt");
+
+    AlignmentSegment* lastSegment = nullptr;
+    Aligner aligner;
+    aligner.setSamples(samples);
+
+     int segCount=0;
+     while (query.next()) {
+
+        string sampleName = query.value("name").toString().toStdString();
+        int sampleId =   query.value("sampleId").toString().toInt();
+        mzSample* sample = sampleById.at(sampleId);
+        if (!sample) continue;
+        segCount++;
+
+        AlignmentSegment* seg = new AlignmentSegment();
+        seg->sampleName   = sampleName;
+        seg->seg_start = 0;
+        seg->seg_end   = query.value("rt").toString().toFloat();
+        seg->new_start = 0;
+        seg->new_end   = query.value("rt_update").toString().toFloat();
+
+       if (lastSegment and lastSegment->sampleName == seg->sampleName) {
+           seg->seg_start = lastSegment->seg_end;
+           seg->new_start = lastSegment->new_end;
+       } else {
+            cout << endl;
+       }
+
+        //debugging
+        cout << sample->sampleName << "\t" << seg->seg_end << "\t" << seg->new_end << endl;
+
+        aligner.addSegment(sample->sampleName,seg);
+        lastSegment = seg;
+       }
+
+     aligner.doSegmentedAligment();
+
+    /*
+     * Taken from ProjectDockWidget::loadAllPeakTables()
+     */
+
+
     cout << "Finished importing mzrolldB file: '" << mzrollDBFile << "'" << endl;
+
 
     cout << "Test Passed" << endl;
 }
