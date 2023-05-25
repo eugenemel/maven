88a883ac093ab227e6450dc75241bdf388de1acd
Phillip Seitzer <phillipseitzer@calicolabs.com>
2023-05-25 10:14:32 -0700

    Issue 646: Begin working on parsing command line arguments.

--- src/isotopeprocessor/isotopeprocessor.cpp
+++ src/isotopeprocessor/isotopeprocessor.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <omp.h>
 
+#include "isotopicenvelopeutils.h"
+
 using namespace std;
+
+void processOptions(int argc, char* argv[]);
 
 int main(int argc, char *argv[]){
 
@@ -11,5 +15,16 @@ int main(int argc, char *argv[]){
 
         #pragma omp critical
         cout << "Hello from thread #" << omp_get_thread_num() << " (nthreads=" << omp_get_num_threads() << ")"<< endl;
+    }
+}
+
+void processOptions(int argc, char* argv[]) {
+
+    IsotopeProcessorOptions& options = IsotopeProcessorOptions::instance();
+
+    for (unsigned int i = 0; i < static_cast<unsigned int>(argc-1); i++) {
+        if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--config") == 0) {
+            options.setOptions(argv[i+1]);
+        }
     }
 }
