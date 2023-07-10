2c2786a26e711073462b71f6083a92046f356c72
Phillip Seitzer <phillipseitzer@calicolabs.com>
2023-07-10 16:31:25 -0700

    Issue 659: Update ExperimentAnchorPoints::compute() method to include second argument.

--- src/peakdetector/peakdetector.cpp
+++ src/peakdetector/peakdetector.cpp
@@ -2355,7 +2355,7 @@ void anchorPointsBasedAlignment() {
                 eic_smoothingWindow,
                 standardsAlignment_minPeakIntensity
                 );
-    experimentAnchorPoints.compute(false);
+    experimentAnchorPoints.compute(false, true); // (debug, isClean)
     sampleToUpdatedRts = experimentAnchorPoints.sampleToUpdatedRts;
 
     printf("Execution time (anchorPointsBasedAlignment()) : %f seconds \n", getTime() - startLoadingTime);
