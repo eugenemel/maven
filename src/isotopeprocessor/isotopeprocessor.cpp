82942a684b7359090993b471d053f9c04b5eeab5
Phillip Seitzer <phillipseitzer@gmail.com>
2023-09-25 15:21:59 -0700

    Issue 671: Delete unused and deprecated parts of isotopeprocessor.

--- src/isotopeprocessor/isotopeprocessor.cpp
+++ src/isotopeprocessor/isotopeprocessor.cpp
@@ -25,17 +25,11 @@ shared_ptr<IsotopicExtractionParameters> params = shared_ptr<IsotopicExtractionP
 void processOptions(int argc, char* argv[]);
 void printUsage();
 
-/**
- * @brief loadSamples
- * @deprecated
- */
-void loadSamples();
 void loadSeedPeakGroups();
 
 int main(int argc, char *argv[]){
     processOptions(argc, argv);
 
-    //loadSamples(); //multithreaded. TODO: ensure that sample IDs match in DB
     project->loadSamples(QString(sampleDir.c_str())); //single-threaded, ensure that IDs match
 
     loadSeedPeakGroups();
@@ -99,11 +93,6 @@ int main(int argc, char *argv[]){
     map<QString, QString> parametersData{make_pair(key, value)};
     project->savePeakGroupsTableData(parametersData);
 
-    // TODO: output directory is currently unused, instead, values are saved to mzrollDB.
-    if (outputDir != "") {
-        //TODO
-    }
-
     cout << "All Processes Completed Successfully!" << endl;
 }
 
@@ -122,8 +111,6 @@ void processOptions(int argc, char* argv[]) {
             project = new ProjectDB(QString(projectFile.c_str()));
         } else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--sampleDir") == 0) {
             sampleDir = argv[i+1];
-        } else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--outputDir") == 0) {
-            outputDir = argv[i+1];
         } else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--adductsFile") == 0) {
             adductsFile = argv[i+1];
             DB.adductsDB = Adduct::loadAdducts(adductsFile);
@@ -149,57 +136,13 @@ void printUsage() {
          << "\t\tConfiguration file with algorithm options.\n"
          << "\t-m [--mzrollDB] <mzrolldb_file>\n"
          << "\t\tmzrollDB file containing peakgroups for envelope extraction.\n"
+         << "\t\tisotopic peak groups will be addd as new 'isotope processor' table of results.\n"
          << "\t-s [--sampleDir] <sample_dir>\n"
          << "\t\tDirectory containing sample files referenced in mzrollDB file.\n"
-         << "\t-o [--outputDir] <output_dir>\n"
-         << "\t\tDirectory to write all output files to.\n"
          << "\t\tWhether or not the argument is provided, envelopes will be saved into the original mzrollDB file.\n"
          << "\t-a [--adductsFile] <adducts_file>\n"
          << "\t\tFile containing adducts information, usually ADDUCTS.csv\n"
          << endl;
-}
-
-/**
- * @brief loadSamples
- * @deprecated
- * this was a multi-threaded implementation, however, this doesn't make any sense if
- * the input is always an mzrollDB file.
- */
-void loadSamples() {
-    vector<string> filenames = mzUtils::getMzSampleFilesFromDirectory(sampleDir.c_str());
-
-    cout << "Loading " << filenames.size() << " samples..." << endl;
-
-    vector<mzSample*> samples{};
-
-    #pragma omp parallel for ordered num_threads(16) schedule(dynamic)
-    for (unsigned long i = 0; i < filenames.size(); i++ ) {
-
-        #pragma omp critical
-        //cout << "Thread # " << omp_get_thread_num() << ": Loading " << filenames[i] << endl;
-
-        mzSample* sample = new mzSample();
-        sample->loadSample(filenames[i].c_str());
-
-        if ( sample->scans.size() >= 1 ) {
-
-            #pragma omp critical
-            samples.push_back(sample);
-
-            #pragma omp critical
-            sample->summary();
-
-        } else {
-            if (sample) {
-                delete sample;
-                sample = nullptr;
-            }
-        }
-    }
-
-    project->samples = samples;
-
-    cout << "loadSamples() done: loaded " << project->samples.size() << " samples\n";
 }
 
 /**
