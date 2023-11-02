3daabb6f7826123644865f3e832a374e38c11091
Phillip Seitzer <phillipseitzer@gmail.com>
2023-11-01 17:13:36 -0700

    Issue 681: Update travis os x build configuration steps.

--- ci/travis/before_install_osx.sh
+++ ci/travis/before_install_osx.sh
@@ -9,8 +9,15 @@ brew link qt5 --force
 ENVFILE=qt-5.env
 echo Create $ENVFILE
 cat << EOF > $ENVFILE
-export PATH="/usr/local/opt/qt/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/qt/lib"
-export CPPFLAGS="-I/usr/local/opt/qt/include"
-export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig"
+
+# OLD as of issue #681
+# export PATH="/usr/local/opt/qt/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/qt/lib"
+# export CPPFLAGS="-I/usr/local/opt/qt/include"
+# export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig"
+
+export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/qt@5/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/qt@5/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/pkgconfig/"
 EOF
