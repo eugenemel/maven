9dced61cd5d2df67ffe74f9fc17c7c20e9e37c80
Phillip Seitzer <phillipseitzer@gmail.com>
2023-11-02 10:55:35 -0700

    Issue 681: Removed comments.

--- ci/travis/before_install_osx.sh
+++ ci/travis/before_install_osx.sh
@@ -10,12 +10,6 @@ ENVFILE=qt-5.env
 echo Create $ENVFILE
 cat << EOF > $ENVFILE
 
-# OLD as of issue #681
-# export PATH="/usr/local/opt/qt/bin:$PATH"
-# export LDFLAGS="-L/usr/local/opt/qt/lib"
-# export CPPFLAGS="-I/usr/local/opt/qt/include"
-# export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig"
-
 export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"
 export LDFLAGS="-L/opt/homebrew/opt/qt@5/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/qt@5/include"
