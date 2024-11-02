6efbce582a3a3b0cef9ee602e529105013e62867
Phillip Seitzer <phillipseitzer@gmail.com>
2024-11-02 09:55:36 -0700

    Issue 762: Try installing cmake separately.

--- ci/travis/before_install_osx.sh
+++ ci/travis/before_install_osx.sh
@@ -2,9 +2,10 @@
 
 #echo "Maven debugging: try doing nothing for mac os x"
 
-# travis_wait 30 brew update
-# travis_wait 60 brew install qt@5
-# brew link qt5 --force
+brew update
+brew install cmake
+brew install qt@5
+brew link qt5 --force
 
 ENVFILE=qt-5.env
 echo Create $ENVFILE
